e653817ff7e03603355157c6d98bc32d8336f5b3
Prashant Chauhan <imtej08@gmail.com>
2025-05-04 11:47:52 +0530

    first commit

---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,29 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
+
+const GameGrid = () => {
+  const { data, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={3}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {data.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
+
+export default GameGrid;
