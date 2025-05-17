def6fe1866f12b0423882c351b815d0639558362
Prashant Chauhan <imtej08@gmail.com>
2025-05-17 11:17:32 +0530

    Set up routes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,18 +7,16 @@ import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <div>
-      <HStack padding="10px">
-        <Show below="lg">
-          <GenreDrawerMenu />
-        </Show>
-        <Show above="lg">
-          <Image src={logo} boxSize="60px" />
-        </Show>
-        <SearchInput />
-        <ColorModeSwitch />
-      </HStack>
-    </div>
+    <HStack padding="10px">
+      <Show below="lg">
+        <GenreDrawerMenu />
+      </Show>
+      <Show above="lg">
+        <Image src={logo} boxSize="60px" />
+      </Show>
+      <SearchInput />
+      <ColorModeSwitch />
+    </HStack>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import { RouterProvider } from "react-router-dom";
 import theme from "./components/theme.ts";
-import App from "./App.tsx";
+import router from "./routes.tsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
