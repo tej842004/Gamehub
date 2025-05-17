fe60e1716516294469f1d7cea2177a330012b380
Prashant Chauhan <imtej08@gmail.com>
2025-05-17 12:05:23 +0530

    Improve the layout for the mobile screen

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner } from "@chakra-ui/react";
+import { Box, Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
@@ -7,15 +7,25 @@ const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
-  if (isLoading) return <Spinner />;
+  if (isLoading)
+    return (
+      <Box
+        display="flex"
+        height="50vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Spinner />
+      </Box>
+    );
 
   if (error || !game) throw error;
 
   return (
-    <>
+    <Box padding={5}>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
-    </>
+    </Box>
   );
 };
 
